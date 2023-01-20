a8a82c50159148163a1f2d8397929ce5e91d7319
ddogga <hayeonlee60327@gmail.com>
2023-01-20 11:03:15 +0900

    JPQL 예제 프로젝트 : 프로젝션 - 여러 값 조회 예제

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -25,21 +25,32 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            // 엔티티 프로젝션
-            List<Member> result1 = em.createQuery("select m from Member m", Member.class)
+
+            // Object[] 타입으로 변환
+            List resultList = em.createQuery("select m.username, m.age from Member m")
                     .getResultList();
 
-            //(참조(?))엔티티 프로젝션
-            List<Team> result2 = em.createQuery("select m.team from Member m join m.team", Team.class)
+            Object o = resultList.get(0);
+            Object[] result = (Object[]) o;
+            System.out.println("username = " + result[0]);
+            System.out.println("age = " + result[1]);
+
+            // Object[] 타입으로 조회
+            List<Object[]> resultList1= em.createQuery("select m.username, m.age from Member m")
                     .getResultList();
 
-            //임베디드 타입 프로젝션
-            em.createQuery("select o.address from Order o", Address.class)
+            Object[] result1 = resultList1.get(0);
+            System.out.println("username = " + result1[0]);
+            System.out.println("age = " + result1[1]);
+
+
+            //new 명령어로 조회
+            List<MemberDTO> resultList2= em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m, MemberDTO.class")
                     .getResultList();
 
-            //스칼라 타입 프로젝션
-            em.createQuery("select m.username, m.age from Member m")
-                    .getResultList();
+            MemberDTO memberDTO = resultList2.get(0);
+            System.out.println("memberDTO = " + memberDTO.getUsername());
+            System.out.println("memberDTO = " + memberDTO.getAge());
 
 
            tx.commit();
