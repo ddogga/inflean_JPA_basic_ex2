e6195f0fcbd558a8e3f2681f62f2ef44bdaeb7d0
ddogga <hayeonlee60327@gmail.com>
2023-01-20 17:47:55 +0900

    JPQL 예제 프로젝트 : CASE식 예제

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -34,13 +34,20 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            // enum 타입을 조건으로 검색 -> 아래와 같이 enum명과 패키지까지 다 적어 줘야 함.
-            String query = "select m from Member m " +
-                    "where m.type = :userType";
-
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
-                    .getResultList();
+            String query =
+                    "select " +
+                            " case when m.age <= 10 then '학생요금' " +
+                            " when m.age >= 60 then '경로요금' " +
+                            " else '일반요금' " +
+                            " end " +
+                    "from Member m";
+            
+            List<String> result = em.createQuery(query, String.class)
+                            .getResultList();
+            
+            for (String s: result) {
+                System.out.println("s = " + s);
+            }
 
            tx.commit();
 
