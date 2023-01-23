1ff5d102af38222ae311bddd9048f8a6d1c9030e
ddogga <hayeonlee60327@gmail.com>
2023-01-23 16:25:10 +0900

    JPQL 예제 프로젝트 : 페치조인과 DISTINCT

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -43,12 +43,15 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t From Team t join fetch t.members";
+            String query = "select distinct t From Team t join fetch t.members";
 
             List<Team> result = em.createQuery(query,Team.class).getResultList();
 
             for (Team team : result) {
                 System.out.println("team = " + team.getName() + "|members=" + team.getMembers().size());
+                for(Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
             }
 
 
